f2fa60d321314a58ec037df822a3b5c10764c418
Ashwikk <ashwikk045@gmail.com>
2024-07-16 11:35:01 +0530

    add

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+const userRoutes = require('./routes/users');
+const postRoutes = require('./routes/posts');
+const commentRoutes = require('./routes/comments');
+
+app.use('/users', userRoutes);
+app.use('/posts', postRoutes);
+app.use('/posts/:postId/comments', commentRoutes);
+
+app.listen(3000, () => {
+    console.log('Server started on port 3000');
+});
